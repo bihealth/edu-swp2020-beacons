fb019f5eef49262ce238dc3d5564a78a22391442
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2020-03-24 06:29:06 +0100

    Adding module and tests.

---
+++ tests/test_sub.py
@@ -0,0 +1,7 @@
+import pytest
+
+from the_module import sub
+
+
+def test_f():
+    assert int(sub.f(2)) == 4
---
+++ the_module/sub.py
@@ -0,0 +1,5 @@
+"""Just an example test module."""
+
+
+def f(x):
+    return x * x + 0.5
