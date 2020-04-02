d2665bd8d476f61aab236dd414945e5af59f690a
LB528 <61065092+LB528@users.noreply.github.com>
2020-04-02 16:35:13 +0200

    Update common.py

--- the_module/beacon/common.py
+++ the_module/beacon/common.py
@@ -1,1 +1,24 @@
+class Variant:
+    """Input variants"""
+    def __init__(self, chr, pos, res, alt):
+        self.chr = chr
+        self.pos = pos
+        self.res = res
+        self.alt = alt
 
+class AnnotatedVariant():
+    """Output true or false"""
+    def __init__(self, var, occ):
+        self.var = var
+        self.occ = occ
+        
+class VariantStringParser:
+    """ variant object parses an input string to a variant object or variant 
+        objectto string """
+    def __init__(self, var_str):
+        pass
+    def parse_var(self, var_str): 
+        """Input: variant string 
+        Output: variant object parses an input string to a variant object or variant 
+        objectto string"""
+        pass
