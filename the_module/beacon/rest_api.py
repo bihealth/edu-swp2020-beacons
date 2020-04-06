8ec18844f45f060a612bfad7751c938e7abc37f8
whywhowhat <jin.soo.park.1991@gmail.com>
2020-04-06 09:12:10 +0200

    Update rest_api.py

--- the_module/beacon/rest_api.py
+++ the_module/beacon/rest_api.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 
 class Api:
     def __init__(self):
-
+        #API input
+        self.chr = chr
+        self.pos=pos
+        self.res = res
+        self.alt = alt
         pass
 
     def annVar_ls():
@@ -14,4 +18,20 @@ class Api:
                    	        Output: ls with api values and AnnotatedVariant class  variables
                    	        function transformes AnnotatedVariant to ls
                    	        api =[{“chr”:str,“pos”:int,“res”:chr,“alt”:chr,“occ”:bool}]"""
+        def checker():
+            #check first whether our amied Variants in our Databank 
+         while True:
+              return False
+        
         pass
+    class AnnotatedVariant():
+    """
+    Output true or false"""
+    def __init__(self, var, occ):
+        self.var = var
+        self.occ = occ
+
+    def checker():
+         while True:
+              return False
+
