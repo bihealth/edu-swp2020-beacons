8fd75c0aabfccfbabbe745163760dda9ad4fc021
Namuun <namuun@email.de>
2020-04-08 16:31:18 +0200

    admin_cli interagiert vollständig mit admin_tools

--- the_module/beacon/admin_cli.py
+++ the_module/beacon/admin_cli.py
@@ -2,96 +2,67 @@
  
 import sqlite3
 from sqlite3 import Error
-import admin_cli
+import admin_tools
 import argparse
-#import beacon.common
 import database
-import pyvcf
-import argparse
+import vcf, sys, os 
 
-def __main__(args):
+def main():
     
-    connectparser = argparse.ArgumentParser(prog='admin', fromfile_prefix_chars='@')
+    parser = argparse.ArgumentParser()
     parser.add_argument("-ct","--create_table", 
-                        help="according to a given sql command it creates new table in database",
-                        action="store_true", 
-                        nargs=1
-                        )
-    parser.add_argument("-vcf","--insert_data", 
+                        action = 'store_true',
+                        help="according to a given sql command it creates new table in database")
+    parser.add_argument("-vcf","--insert_data",
                         help="according to a given sql command it inserts data from a vcf file in database",
-                        action="store_true", 
-                        nargs='?', 
-                        type=file,
-                        required=True
-                        )
-    parser.add_argument("-fd","--find_dup", 
-                        help="according to a given sql command it finds duplicated data in database",
-                        action="store_true", 
-                        nargs=1)
-    parser.add_argument("-p","--print_db", 
-                        help="according to a given sql command it prints the database",
-                        action="store_true", 
-                        nargs=1)
-    parser.add_argument("-c","--count_variants", 
-                        help="according to a given sql command it counts the variants in database",
-                        action="store_true", 
-                        nargs=1)
+                        type=argparse.FileType('r'))
+    parser.add_argument("-fd","--find_dup",  
+                        action = 'store_true',
+                        help="according to a given sql command it finds duplicated data in database")
+    parser.add_argument("-p","--print_db",  
+                        action = 'store_true',
+                        help="according to a given sql command it prints the database")
+    parser.add_argument("-c","--count_variants",  
+                        action = 'store_true',
+                        help="according to a given sql command it counts the variants in database")
     parser.add_argument("-u","--update", 
-                        help="according to a given sql command it finds duplicated data in database",
-                        action="store_true", 
-                        nargs=5) 
-    parser.add_argument("-d","--delete", 
-                        help="according to a given sql command it finds duplicated data in database",
-                        action="store_true", 
-                        nargs=1)
+                        help="according to a given sql command it update data in database",
+                        nargs = 5) 
+    parser.add_argument("-d","--delete",  
+                        help="according to a given sql command it deletes data in database")
 
-
-
-#environmental variable 
-    connect = database.ConnectDatabase(os.environ.get("PATH_DATABASE"))
-
+    #PATH_DATABASE = "/home/namuun/edu-swp2020-beacons/the_module/beacon/database.db"
+    #PATH_DATABASE = input('PATH_DATABASE: ')
+    connect = database.ConnectDatabase('/home/namuun/edu-swp2020-beacons/the_module/beacon/database.db') 
     args = parser.parse_args()
-
+    od = admin_tools.OperateDatabase()
     if args.create_table:
         print("create table is activated")
-        print(admin_tools.create_tables(connect))
-
-    if args.insert_data: 
+        create = admin_tools.CreateDbCommand()
+        print(create.create_tables(connect))
+        #print(admin_tools.create_tables(connect))
+    elif args.insert_data:
         print("inserting data is activated")
-        infile = input("vcf file: ")
-        print(parse_vcf(infile))
-
-    if args.find_dup:
-        print("create table is activated")
-        print(admin_tools.find_dup(connect))
-
-    if args.print_db:
-        print("create table is activated")
-        print(admin_tools.print_db(connect))
+        print(admin_tools.parse_vcf(args.insert_data, connect))
+    elif args.find_dup:
+        print("find_dup is activated")
+        finddup = admin_tools.SearchDuplicatesCommand() 
+        print(finddup.find_dup(connect))
+    elif args.print_db:
+        print("print_db is activated")
+        print(od.print_db(connect))
+    elif args.count_variants:
+        print("count_variants is activated")
+        print(od.count_variants(connect))
+    elif args.update: 
+        print("update is activated")  
+        print(od.updating_data(connect,tuple(args.update)))
+    elif args.delete:
+        print("delete is activated")
+        print(od.delete_data(connect,tuple(args.delete)))
     
-    if args.count_variants:
-        print("create table is activated")
-        print(admin_tools.count_variants(connect))
-
-    if args.update: -u chr pos 
-        print("create table is activated")
-        variants = input("chr, pos, ref, alt, id: ")
-        print(admin_tools.update(connect,variants))
-
-    if args.delete:
-        print("create table is activated")
-        print(admin_tools.delete(connect,id)
-
-
     connect.connection.close()
 
-# total changes von sql 
-
-
-
- 
-#nct: def export_db()
-print("hier angekommen")
-
-if __name__==if __name__ == "__main__":
-    pass
+# print("hier in admin_client angekommen")
+if __name__=="__main__":
+    main()
--- the_module/beacon/admin_tools.py
+++ the_module/beacon/admin_tools.py
@@ -2,126 +2,126 @@ import sqlite3
 from sqlite3 import Error
 #import beacon.common
 import database
-import pyvcf
+import vcf
 import os
 
 #db_connection = DatabaseConnection(path=os.environ.get("/database.db"))
 #wann wollen wir datenbank path festlegen? bei jeder neuen datenbank automatisch oder separiert durch den admint?
 #error handling
 
-
 def parse_vcf(infile, con):
-    vcfdata= open(infile,"r")
-    vcf_reader = vcf.Reader(vcfdata)
+    vcf_reader = vcf.Reader(infile)
     try: 
-        for variable  in vcf_reader:
+        for variant in vcf_reader:
             """give to datenbank (sql_str)"""
-            chr = record.CHROM
-            pos = record.POS
-            ref = record.REF
-            alt = record.ALT
-            con.parse_statement("INSERT INTO variants (id,chr,pos,ref,alt) VALUES (?,?,?,?,?)",(id,chr,pos,ref,alt)) 
-            con.connection.close()
+            chr = variant.CHROM
+            pos = str(variant.POS)
+            res = variant.REF
+            alt = "".join(str(i or '') for i in variant.ALT)
+            #print(alt)
+            sql_str = "INSERT INTO variants (chr,pos,ref,alt) VALUES ("+chr+","+pos+",'"+res+"','"+alt+"');"
+            #print(sql_str)
+            con.parse_statement(sql_str)
+        return True
     except sqlite3.Error as e:
-        print(e)
-    infile.close()
-    return True
+        return e
+    infile.close()   
 
 class CreateDbCommand:
     def __init__(self):
-        self.data = []
+        self.data = [] 
  
     def create_tables(self, con):
         """ Input: Variant
-        Output: bool
-        creates variant table in database """
+       Output: bool
+       creates variant table in database """
         try:
-            sql_create_db_table = """ CREATE TABLE IF NOT EXISTS  (
-                                        id integer PRIMARY KEY,
+            sql_create_db_table = """ CREATE TABLE IF NOT EXISTS variants (
+                                        id integer PRIMARY KEY AUTOINCREMENT,
                                         chr text NOT NULL,
                                         pos integer NOT NULL,
                                         ref text NOT NULL,
                                         alt text NOT NULL
-                                    ); """                         
-            output = con.parse_statement(sql_create_db_table)
+                                    ); """        
+            output = con.parse_statement(sql_create_db_table) 
+            return True
         except sqlite3.Error as e:
-            print(e)
-        return True
+            return e
         
-
 class SearchDuplicatesCommand:
     def __init__(self):
         self.data = []
 
     def find_dup(self, con):
         """ Input: database
-        Output: bool
+        Output: list of duplicates
         looks for duplicates"""
         try:
-            sql_find_dup = "SELECT id, chr, pos, COUNT(*) FROM variants GROUP BY chr, pos, ref, alt HAVING COUNT(*) > 1"                         
+            #sql_find_dup = "SELECT DISTINCT chr, pos, ref, alt FROM variants ORDER BY chr;"  
+            sql_find_dup = "SELECT id, chr, pos, COUNT(*) FROM variants GROUP BY chr, pos, ref, alt HAVING COUNT(*) > 1;"
             output = con.parse_statement(sql_find_dup)
             for out in output:
-                print(out + "\n")
+                print(out)
+            return ""
         except sqlite3.Error as e:
-            print(e)
-        return True
- 
+            return e
+
 class OperateDatabase:
     def __init__(self):
         self.data = []
-        
+       
     def print_db(self, con):
         """prints whole Database"""
         try:
             sql_print = "SELECT id, chr, pos, ref, alt FROM variants GROUP BY id, chr, pos, ref, alt"                         
             output = con.parse_statement(sql_print)
             for out in output:
-                print(out + "\n")
+                print(out)
+            return ""
         except sqlite3.Error as e:
-            print(e)
-        return True
+            return e
  
     def count_variants(self, con):
         """ counts the existing number of (all) Variants """
         try:
             sql_count_var = "SELECT COUNT(*) FROM variants"                         
             output = con.parse_statement(sql_count_var)
-            for out in output:
-                print(out + "\n")
+            return (int(output[0][0]))
         except sqlite3.Error as e:
-            print(e)
-        return True
-	
+            return e
+
     def updating_data(self, con, variants):
         """Input:variants
         Output:bool
         function access the database using connect() and updates."""
         try:
-            sql = ''' UPDATE variants
-                    SET chr = ? ,
-                        pos = ? ,
-                        ref = ? ,
-                        alt = ? 
-                    WHERE id = ? '''  
-            forparse = (sql, variants)                      
-            output = con.parse_statement(sql)
+            chr = variants[0]
+            pos = str(variants[1])
+            res = variants[2]
+            alt = "".join(str(i or '') for i in variants[3]) 
+            id = variants[4]  
+            sql_str = "UPDATE variants SET chr = "+chr+", pos = "+pos+" , ref = '"+res+"' , alt = '"+alt+"' WHERE id = "+id+""                         
+            output = con.parse_statement(sql_str)
+            #print_db(con)
+            return "rufe -p auf, um die Änderung zu sehen"
         except sqlite3.Error as e:
-            print(e)
-        return True
-
+            return e
 
     def delete_data(self, con, id):
         """ Input: Variant, Database
         Output: bool
         function deletes given Variant in db and gives bool if succeeded back"""
         try: 
-            sql = 'DELETE FROM variants WHERE id=?'
-            forparse = (sql, (id,))
-            output = con.parse_statement(forparse)
-            return True
+            id = id[0]
+            sql_str = "DELETE FROM variants WHERE id= "+id+""
+            output = con.parse_statement(sql_str)
+            return "rufe -p auf, um die Änderung zu sehen"
         except sqlite3.Error as e:
-            print(e)
-        return True
+            return e
  
 #nct: def export_db() + def print_row() + def count
-print("hier angekommen")
+# für version 2:
+# - Kommas einlesen, bei alt in der funktion parse_vcf 
+# - bei der funktion find_dup -> automatisch löschen 
+# - 5 x updadte variablen benennen bei -h update -u U U U U U
+# - in der funktion update und delete noch das ergebnis printen
