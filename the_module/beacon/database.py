4e00f87c2913f9d4e3b72c387fe7d016d5983f6e
Julia <Julia@email.com>
2020-04-09 09:56:57 +0200

    aktuelle database

--- the_module/beacon/database.py
+++ the_module/beacon/database.py
@@ -1,6 +1,6 @@
 ## module beacon.database
 import sqlite3
-import common as cm
+from . import common 
 
 class ConnectDatabase:
 
@@ -25,14 +25,14 @@ class ConnectDatabase:
             else:
                 return output
         except sqlite3.Error as e:
-            return False
+            return e
 
     def handle_variant(self,variant):
         """Input: Variant Object
       	   Output: AnnotatedVariant(bool)
      	   Gets an variant object from flask handle and parses request to database and gets an  Annotated as an output """
         try:
-            sql_str = "SELECT id FROM variants WHERE chr = "+variant.chr+" AND pos = "+variant.pos+" AND res = "+variant.res+" AND alt = "+variant.alt+";"
+            sql_str = "SELECT id FROM variants WHERE chr = '"+variant.chr+"' AND pos = "+variant.pos+" AND res = '"+variant.res+"' AND alt = '"+variant.alt+"';"
             occ = self.parse_statement(sql_str, True)
             return occ
         except sqlite3.Error as e:
