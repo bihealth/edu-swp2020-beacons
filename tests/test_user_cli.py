0e68ec9b258c6a3ee650e753ea50457c5f024e11
Leo <leo@blamail.de>
2020-04-16 14:49:20 +0200

    blabla2

--- tests/test_user_cli.py
+++ tests/test_user_cli.py
@@ -1,26 +1,24 @@
 import pytest
 from the_module.beacon import user_cli
 import requests_mock
+
 inputs = [("1-1-A-A",True), ("3-4-X-T",False),("1.1.A.T.",False)]
-inputs2 = [("c", ""),("q",""),("x","")]
-#
-#@pytest.fixture
-#def testserver():
-#    from flask import Flask
-#    app = Flask(__name__)
-#    @app.route("/api/<var_str>", methods = ['GET'])
-#    def get_api(var_str):
-#        var = common.parse_var(var_str)
-#        occ = True
-#        return(jsonify(results=annVar_ls,o
-#    
+inputs2 = [("1-1-A-A","q"),("3-4-X-T","q"),("1.1.A.T.","x")]
 
 
+
+@pytest.mark.parametrize("var,sig", inputs2)
 @requests_mock.Mocker(kw='mock')
-def test_init(monkeypatch, capsys, **kwargs):
+def test_init(var,sig, monkeypatch, capsys, **kwargs):
     kwargs['mock'].get('http://localhost:5000/api/1-1-A-A', json = {'results':['1','1','A','A','True']})
-    monkeypatch.setattr(user_cli, '_check_input', lambda x: True)
-    monkeypatch.setattr('builtins.input', lambda x: '1-1-A-A')
+    #monkeypatch.setattr(user_cli, '_check_input', lambda x: True)
+    def mock_input(x):
+        if x == "Please enter your variant (chr-pos-ref-alt):\n":
+            return var
+        else:
+            return sig
+
+    monkeypatch.setattr('builtins.input', mock_input)
     user_cli.main()
 
     captured = capsys.readouterr()
