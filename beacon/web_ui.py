b2ed017dbd90180173adc6fb61574296d22a1130
Leylanur <leylanur@email.de>
2020-05-12 13:21:24 +0200

    web_ui

--- beacon/web_ui.py
+++ beacon/web_ui.py
@@ -1,6 +1,6 @@
 from . import settings
 from . import database
-from flask import Flask, request, render_template, url_for #json
+from flask import Flask, request, render_template, url_for  # json
 import requests
 import webbrowser
 import base64
@@ -41,7 +41,7 @@ def handle():
         "http://localhost:5000/query", json=jform, headers={"token": token}
     )
     res = rep.json()
-    if  len(res) > 6:
+    if len(res) > 6:
         atable = True
         if res["statistic"] is None:
             static = False
@@ -81,6 +81,7 @@ def handle():
         )
     return render_template("output.html", title="Results", **locals())
 
+
 @app.route("/login", methods=["POST", "GET"])
 def login():
     """
@@ -88,20 +89,28 @@ def login():
     Output: Home-Page with input fields and submit button
     """
     if request.method == "POST":
-        token = request.form["token"]
-        resp = requests.post(
-            "http://localhost:5000/api/verify", headers={"token": token}
-        )
-        if resp.json()["verified"]:
-            username = resp.json()["user"]
-            return render_template("home.html", token=token, user=username)
-        else:
-            error = "This is not a valid token"
+        try:
+            token = request.form["token"]
+            resp = requests.post(
+                "http://localhost:5000/api/verify", headers={"token": token}
+            )
+            if resp.json()["verified"]:
+                username = resp.json()["user"]
+                return render_template("home.html", token=token, user=username)
+            elif resp.json()["verified"] == False:
+                error = "This is not a valid token"
+                return render_template("login.html", title="Login", error=error)
+            else:
+                error = resp.json()["error"]
+                return render_template("login.html", title="Login", error=error)
+        except Exception as e:
+            error = "We have troubles reaching the server"
             return render_template("login.html", title="Login", error=error)
+
     else:
         return render_template("login.html", title="Login")
 
 
 if __name__ == "__main__":
-    webbrowser.open_new("http://localhost:4000/")
-    app.run(debug=True, port="4000")
+    webbrowser.open_new("http://localhost:4000/")  # pragma: no cover
+    app.run(debug=True, port="4000")  # pragma: no cover
